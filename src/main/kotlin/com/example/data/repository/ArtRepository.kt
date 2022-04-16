3b157377e99ab2219b14bbf777440409c1e7792b
Ian Okumu <okumu.otsembo@gmail.com>
2022-04-16 21:58:22 +0300

    Repository: Correct mapping of sql query

--- src/main/kotlin/com/example/data/repository/ArtRepository.kt
+++ src/main/kotlin/com/example/data/repository/ArtRepository.kt
@@ -4,6 +4,8 @@ import com.example.model.Art
 import com.example.model.ArtEntity
 import com.example.model.ArtPieces
 import com.example.model.getArt
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.greater
+import org.jetbrains.exposed.sql.and
 import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.selectAll
 import org.jetbrains.exposed.sql.transactions.transaction
@@ -13,7 +15,7 @@ class ArtRepository {
     fun getLatestArt() : List<Art>{
         val artList =  ArrayList<Art>()
         transaction {
-            ArtEntity.find { ArtPieces.isActive eq false  }.limit(5).forEach {
+            ArtEntity.find { (ArtPieces.isActive eq true)  and (ArtPieces.stock greater 0)  }.limit(5).forEach {
                 artList.add(
                     it.getArt()
                 )
